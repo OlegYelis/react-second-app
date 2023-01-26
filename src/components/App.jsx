4c3fcfaa55e12a8a4d930f2b34660069d53d08e4
Oleg Yelisieiev <dodgeoleg@gmail.com>
2023-01-26 18:13:57 +0200

    using the useEffect hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Home } from './Home/Home';
 import { Login } from './Login/Login';
 import { MainHeader } from './MainHeader/MainHeader';
@@ -6,11 +6,21 @@ import { MainHeader } from './MainHeader/MainHeader';
 export const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLoginInfo = localStorage.getItem('isLoggedIn');
+
+    if (storedLoginInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
